9af999c974b6ecc85d1875f5cc8d6a38a7180245
Akmal Khamdullaev <akm992@mai.ru>
2026-04-01 12:13:47 +0300

    coursework_5 done

--- habit_tracker/habits/urls.py
+++ habit_tracker/habits/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import HabitViewSet, PublicHabitListView
+from .views import (
+    HabitViewSet,
+    PublicHabitListView,
+    RegisterView,
+    CustomTokenObtainPairView,
+    UserDetailView
+)
 
 router = DefaultRouter()
 router.register(r'habits', HabitViewSet, basename='habit')
@@ -9,4 +16,8 @@ router.register(r'habits', HabitViewSet, basename='habit')
 urlpatterns = [
     path('', include(router.urls)),
     path('public/', PublicHabitListView.as_view(), name='public-habits'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
 ]
